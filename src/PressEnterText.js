c08510c7f788abc20b069a2f1abf147dc06e16c8
Nauris Kolmanis <nauris.kolmanis@gmail.com>
2019-09-27 03:40:16 +0300

    Change press enter text

--- src/PressEnterText.js
+++ src/PressEnterText.js
@@ -13,10 +13,10 @@ PressEnterText.prototype.draw = function (ctx) {
   
   ctx.fillStyle = "red";
   ctx.font = `bold ${calculateScale(18)}px 'Lucida Console', Monaco, monospace`
-  var text = "PRESS ENTER";
+  var text = "PRESS 1: ANSIS, 2: KRISTINE, 3: EDAVARDI";
   var textWidth = ctx.measureText(text).width;
   // Draw text in the center of the canvas.
-  var x = ctx.canvas.width / 2 - textWidth / 2;
-  var y = ctx.canvas.height / 2;
+  var x = parseInt(ctx.canvas.style.width) / 2 - textWidth / 2;
+  var y = parseInt(ctx.canvas.style.height) / 2;
   ctx.fillText(text, x, y);
 };
