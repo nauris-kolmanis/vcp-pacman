0b649469b601c7fa5554a0ab61e6686cf5aa5bcb
Nauris Kolmanis <nauris.kolmanis@gmail.com>
2019-10-02 00:46:45 +0300

    Avoid loading non-existing images

--- src/ImageManager.js
+++ src/ImageManager.js
@@ -1,84 +1,119 @@
-var CUSTOMIZED_IMAGE_PREFIX = 'VCP';
+var NAME_PREFIX = 'VCP';
 
 var ImageManager = (function() {
   var images = {};
 
-  var names = [
-   'wall_h',
-   'wall_v',
-   'wall_tlc',
-   'wall_trc',
-   'wall_blc',
-   'wall_brc',
-   'wall_t',
-   'wall_b',
-   'wall_l',
-   'wall_r',
-   'wall_mt',
-   'wall_mb',
-   'wall_ml',
-   'wall_mr',
+  var imageSetNames = [
+    'pacman_1',
+    'pacman_2l',
+    'pacman_3l',
+    'pacman_2r',
+    'pacman_3r',
+    'pacman_2u',
+    'pacman_3u',
+    'pacman_2d',
+    'pacman_3d',
+      
+    'pacman_dies_1',
+    'pacman_dies_2',
+    'pacman_dies_3',
+    'pacman_dies_4',
+    'pacman_dies_5',
+    'pacman_dies_6',
+    'pacman_dies_7',
+    'pacman_dies_8',
+    'pacman_dies_9',
+    'pacman_dies_10',
+    'pacman_dies_11',
+    'pacman_dies_12',
+      
+    //  'blinky_1',
+    //  'blinky_2',
+      
+    'pinky_1',
+    'pinky_2',
+    'pinky_vulnerable_1',
+    'pinky_vulnerable_2',
+    'pinky_vulnerable_1b',
+    'pinky_vulnerable_2b',
+      
+    'inky_1',
+    'inky_2',
+    'inky_vulnerable_1',
+    'inky_vulnerable_2',
+    'inky_vulnerable_1b',
+    'inky_vulnerable_2b',
+      
+    'clyde_1',
+    'clyde_2',
+    'clyde_vulnerable_1',
+    'clyde_vulnerable_2',
+    'clyde_vulnerable_1b',
+    'clyde_vulnerable_2b',
     
-   'pacman_1',
-   'pacman_2l',
-   'pacman_3l',
-   'pacman_2r',
-   'pacman_3r',
-   'pacman_2u',
-   'pacman_3u',
-   'pacman_2d',
-   'pacman_3d',
-    
-   'pacman_dies_1',
-   'pacman_dies_2',
-   'pacman_dies_3',
-   'pacman_dies_4',
-   'pacman_dies_5',
-   'pacman_dies_6',
-   'pacman_dies_7',
-   'pacman_dies_8',
-   'pacman_dies_9',
-   'pacman_dies_10',
-   'pacman_dies_11',
-   'pacman_dies_12',
-    
-   'blinky_1',
-   'blinky_2',
-    
-   'pinky_1',
-   'pinky_2',
-    
-   'inky_1',
-   'inky_2',
-    
-   'clyde_1',
-   'clyde_2',
-    
-   'vulnerable_1',
-   'vulnerable_2',
-   'vulnerable_1b',
-   'vulnerable_2b',
-    
-   'eyes_l',
-   'eyes_r',
-   'eyes_u',
-   'eyes_d',
-    
-   'pellet',
-   'power_pellet',
-    
-   'cherry',
+    'power_pellet',
+      
+    'cherry',
   ];
 
-  for (var i in names) {
-    var name = names[i];
+  var genericNames = [
+    'wall_h',
+    'wall_v',
+    'wall_tlc',
+    'wall_trc',
+    'wall_blc',
+    'wall_brc',
+    'wall_t',
+    'wall_b',
+    'wall_l',
+    'wall_r',
+    'wall_mt',
+    'wall_mb',
+    'wall_ml',
+    'wall_mr',
+      
+    'eyes_l',
+    'eyes_r',
+    'eyes_u',
+    'eyes_d',
+      
+    'power_pellet',
+    'pellet',
 
-    names.push(`${CUSTOMIZED_IMAGE_PREFIX}_${name}`);
+    'cherry',
+  ];
 
+  names = imageSetNames.concat(genericNames);
+
+  for (var i in imageSetNames) {
     for (var j in IMAGESET_MAP) {
-      names.push(`${CUSTOMIZED_IMAGE_PREFIX}_${IMAGESET_MAP[j]}_${name}`);
+      names.push(`${NAME_PREFIX}_${IMAGESET_MAP[j]}_${imageSetNames[i]}`);
     }
   }
+
+  for (var i in genericNames) {
+    names.push(`${NAME_PREFIX}_${genericNames[i]}`);
+  }
+
+  var getImage = function (name, scene) {
+    var customizedName = scene._imageSet ? `${NAME_PREFIX}_${scene._imageSet}_${name}` : null;
+
+    if (customizedName !== null && images.hasOwnProperty(customizedName)) {
+      return images[customizedName];
+    }
+
+    customizedName = `${NAME_PREFIX}_${name}`;
+
+    if (scene._imageSet && images.hasOwnProperty(customizedName)) {
+      return images[customizedName];
+    }
+
+    return null;
+  };
+
+  var getOriginalImage = function (name) {
+    return images[name];
+  };
   
   return {
     preloadImages: function (onload) {
@@ -105,34 +140,8 @@ var ImageManager = (function() {
       }
     },
 
-    getImage: function (name) {
-      return images[name];
-    },
-
-    getCustomizedImage: function (name, scene) {
-      var customizedName = scene._imageSet ? `${CUSTOMIZED_IMAGE_PREFIX}_${scene._imageSet}_${name}` : null;
-
-      if (customizedName !== null && images.hasOwnProperty(customizedName)) {
-        return images[customizedName];
-      }
-
-      customizedName = `${CUSTOMIZED_IMAGE_PREFIX}_${name}`;
-
-      if (scene._imageSet && images.hasOwnProperty(customizedName)) {
-        return images[customizedName];
-      }
-
-      return null;
-    },
-
     drawImage: function (ctx, name, x, y, scene) {
-      var originalImage = this.getImage(name);
-      var customizedImage = this.getCustomizedImage(name, scene);
-
-      var width = originalImage.width;
-      var height = originalImage.height;
-
-      ctx.drawImage(customizedImage || originalImage, x, y, calculateScale(width), calculateScale(height));
+      ctx.drawImage(getImage(name, scene) || getOriginalImage(name), x, y, TILE_SIZE, TILE_SIZE);
     }
   };
 })();
